b1485fcdc68796af2cafe109ba245125aab5fe9a
Sebastian Worms <sebastian.worms@uclouvain.be>
2023-03-23 10:41:29 +0100

    - Ajouté une ligne pour permettre aux étudiants de choisir un fichier par GUI - Rajouté une ligne pour bien faire le second graphe avec les valeurs de ratio et pas de pourcentage

--- analyse_sod.R
+++ analyse_sod.R
@@ -12,28 +12,36 @@ setwd("Z:/Encadrement/Assistanat/Q2/LCHM1371/LCHM1371")
 
 sod.df <- read.csv("sample_data.csv")
 
-# On peut vérifier que les données ont du sens, on s'attend à tendre vers 100 quand [SOD] augmente
+# La ligne ci-dessous permet de directement choisir un fichier de données par interface graphique
+
+# sod.df <- read.csv(file.choose())
+
+# On peut vérifier que les données ont du sens, on s'attend à tendre vers 100% quand [SOD] augmente
 
 plot(sod.df$csod, sod.df$inhib)
 
-# Pour la suite je vais transformer les pourcentage d'inhibition en fraction d'inhbition
+# Pour la suite je vais transformer les pourcentage d'inhibition en ratio d'inhbition
 
 sod.df <- sod.df %>%
-  mutate(inhib = inhib/100)
+  mutate(inhib_ratio = inhib/100)
 
 # On suppose que les données sont expliquée par une équation de type inhb = 1 - v0/(V0+[SOD]) et
 # on utilise nls() pour trouver VO NLS a besoin d'une valeur initiale. On peut estimer ça car
 # quand V0 = [SOD] inhib = 0.5. Donc ici V0 a l'air d'etre autour de 1
-sod.nls <- nls(formula = inhib ~ 1 - V0/(V0 + csod), data = sod.df, start = list(V0 = 1))
+sod.nls <- nls(formula = inhib_ratio ~ 1 - V0/(V0 + csod), data = sod.df, start = list(V0 = 1))
 
 # Summary() permet d'accéder à la valeur de V0.
 summary(sod.nls)
 
+
 # On peut rajouter une ligne sur le graphe pour voir si le modèle colle
-V0 <- coef(sod.nls)
-x <- c(0:20)
-y <- 1 - V0/(V0 + x)
-lines(x, y, lty = "dotted", col = "red")
+
+
+V0 <- coef(sod.nls) #Extrait le coefficient du modèle, ici V0
+x <- c(0:20) #Valeurs de [SOD] où on va simuler le modèle
+y <- 1 - (V0/(V0 + x)) #Valeurs d'inhibition simulée
+plot(sod.df$csod, sod.df$inhib_ratio) # Refait le plot avec les ratio plutot que le pourcentage
+lines(x, y, lty = "dotted", col = "red") #Rajoute une ligne
 
 # Une fois V0 trouvé, on peux l'utiliser pour calculer la concentration en unité de nos
 # échantillons en connaissant le %age d'inhibition
